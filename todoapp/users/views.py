6cf54926dda44dfa4ce48aad1fc831f4beebc393
Prabhat Keshari <prabhat.keshari@joshtechnologygroup.com>
2025-02-04 23:58:28 +0530

    Exercise 9 and 10

--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -1,4 +1,15 @@
-class UserRegistrationAPIView():
+import json
+
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics, permissions
+
+from users.serializers import UserRegistrationSerializer
+
+
+class UserRegistrationAPIView(generics.CreateAPIView):
     """
         success response format
          {
@@ -9,12 +20,25 @@ class UserRegistrationAPIView():
            "token"
          }
     """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserRegistrationSerializer
+        
 
-
-class UserLoginAPIView():
+class UserLoginAPIView(APIView):
     """
         success response format
          {
            auth_token: ""
          }
     """
+
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        user = authenticate(username=request.data['email'], password=request.data['password'])
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
+        

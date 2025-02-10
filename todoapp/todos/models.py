e940bf0f2338cb7f38bbaf6928dffdd55a80f023
Prabhat Keshari <prabhat.keshari@joshtechnologygroup.com>
2025-02-11 00:17:56 +0530

    Changes in todos models

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -18,8 +18,8 @@ class Todo(models.Model):
         Add string representation for this model with todos name.
     """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Created by") 
-    name = models.CharField(max_length=1000, verbose_name="Todo name")
-    done = models.BooleanField(default=False, verbose_name="Todo Done")
+    name = models.CharField(max_length=1000, verbose_name="Todo")
+    done = models.BooleanField(default=False, verbose_name="Todo Status")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="Date of Creation")
     date_completed = models.DateTimeField(null=True, blank=True, editable=False, verbose_name="Date of Completion")
 
